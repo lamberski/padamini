b8bb19ee903dbaf88879bed95ffd91ab59f32725
Maciej Lamberski <maciej@lamberski.com>
2012-05-17 17:44:19 +0200

    Update TinyMCE instalation

--- scripts/padamini.js
+++ scripts/padamini.js
@@ -474,26 +474,23 @@ var Padamini = {
   enableTinyMCE: function() {
     if ($("textarea.tinymce").length == 0) return false;
 
-    $('textarea.tinymce').tinymce({
-      // Location of TinyMCE script
-      script_url : '../jscripts/tiny_mce/tiny_mce.js',
-
+    $("textarea.tinymce").tinymce({
       // General options
       theme : "advanced",
-      plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+      plugins : "autoresize,autolink,lists,advimage,inlinepopups,contextmenu,paste,visualchars,template,advlist",
 
       // Theme options
-      theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
-      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
-      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
-      theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+      theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect",
+      theme_advanced_buttons2 : "link,unlink,image,|,forecolor,backcolor,|,bullist,numlist,|,outdent,indent,blockquote,|,removeformat,cleanup,code",
+      theme_advanced_buttons3 : "",
+      theme_advanced_buttons4 : "",
       theme_advanced_toolbar_location : "top",
       theme_advanced_toolbar_align : "left",
-      theme_advanced_statusbar_location : "bottom",
       theme_advanced_resizing : true,
+      theme_advanced_resize_horizontal : false,
 
-      // Example content CSS (should be your site CSS)
-      content_css : "css/content.css",
+      // Custom CSS
+      content_css : "",
 
       // Drop lists for link/image/media/template dialogs
       template_external_list_url : "lists/template_list.js",
