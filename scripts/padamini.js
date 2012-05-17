9df6d40718412cb560df1808690c9838916fde8e
Maciej Lamberski <maciej@lamberski.com>
2012-05-17 16:45:52 +0200

    Install TinyMCE editor

--- scripts/padamini.js
+++ scripts/padamini.js
@@ -22,6 +22,7 @@ var Padamini = {
     Padamini.initModals();
     Padamini.enablePreviewModal();
     Padamini.enableCLEdit();
+    Padamini.enableTinyMCE();
   },
 
   addSubmitStateToButtons: function() {
@@ -437,7 +438,7 @@ var Padamini = {
    * Install CLEdit WYSIWYG plugin on textareas
    */
   enableCLEdit: function() {
-    if ($("textarea.cledit") == 0) return false;
+    if ($("textarea.cleditor").length == 0) return false;
 
     $(".cleditor").each(function() {
       var area = $(this);
@@ -464,6 +465,40 @@ var Padamini = {
           .data("changed", true);
       }
     });
-  }
+  },
 
+  /**
+   * Install TinyMCE WYSIWYG plugin on textareas
+   */
+  enableTinyMCE: function() {
+    if ($("textarea.tinymce").length == 0) return false;
+
+    $('textarea.tinymce').tinymce({
+      // Location of TinyMCE script
+      script_url : '../jscripts/tiny_mce/tiny_mce.js',
+
+      // General options
+      theme : "advanced",
+      plugins : "autolink,lists,pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template,advlist",
+
+      // Theme options
+      theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,styleselect,formatselect,fontselect,fontsizeselect",
+      theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
+      theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+      theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
+      theme_advanced_toolbar_location : "top",
+      theme_advanced_toolbar_align : "left",
+      theme_advanced_statusbar_location : "bottom",
+      theme_advanced_resizing : true,
+
+      // Example content CSS (should be your site CSS)
+      content_css : "css/content.css",
+
+      // Drop lists for link/image/media/template dialogs
+      template_external_list_url : "lists/template_list.js",
+      external_link_list_url : "lists/link_list.js",
+      external_image_list_url : "lists/image_list.js",
+      media_external_list_url : "lists/media_list.js"
+    });
+  }
 };
